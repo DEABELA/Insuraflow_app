502a253260a403e15033b6b368efbc72e03d44d5
DEABELA <94280319+DEABELA@users.noreply.github.com>
2024-03-28 13:46:08 +0300

    footer and renew

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Home from "./Pages/Home";
 import Admin from "./Pages/Admin/Admin";
 import ClaimFront from "./Pages/claim/ClaimFront";
 import ClaimForm from "./Pages/claim/ClaimForm";
+import Renew from "./Pages/Renewal/renew";
 function App() {
   return (
     <Routes>
@@ -16,6 +17,7 @@ function App() {
       <Route path="/admin" element={<Admin />} />
       <Route path="/claim" element={<ClaimFront />} />
       <Route path="/claim form" element={<ClaimForm />} />
+      <Route path="/Renew" element={<Renew />} />
     </Routes>
   );
 }
---
+++ frontend/src/Pages/Renewal/renew.jsx
@@ -0,0 +1,16 @@
+import { NavBar } from "../../components/NavBar";
+import { Footer } from "../../components/Footer";
+
+const Renew = () => {
+    return (
+        <div>
+            <NavBar />
+            <h1 className=" text-center font-bold text-red-700">
+                Renwal
+            </h1>
+            <Footer />
+        </div>
+    );
+};
+
+export default Renew;
